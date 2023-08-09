27a5b5f5df1f746f981a9b5130746b918a6de653
Yuri <yuripeixinho03@gmail.com>
2023-08-08 22:34:56 -0300

    deploy

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,5 +1,6 @@
-
+import os 
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = "postgresql://flaskblog_tcxc_user:Q30OoJjkcCDXsLNyfOy5MSkkZUkd7tDU@dpg-cj9eeiivvtos73dt27fg-a.oregon-postgres.render.com/flaskblog_tcxc"
+    
